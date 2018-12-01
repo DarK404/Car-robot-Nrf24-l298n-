8049391ba227f2776b48fdb8bf0d4047af6c7174
Mohamed khalil <khalil.kahlaoui@gmail.com>
2018-12-01 14:36:55 +0100

    Update transmetter.ino

--- transmetter.ino
+++ transmetter.ino
@@ -11,15 +11,15 @@ void setup(void)
 {
   radio.begin();
   radio.openWritingPipe(pipe);
-  radio.setDataRate(RF24_250KBPS);
+  //radio.setDataRate(RF24_250KBPS); // Recommended use of the capacitor in case of activating this function
   radio.setRetries(15, 15);
   Serial.begin(9600);
 }
 void loop() {
   datos[0] = analogRead(1);
   datos[1] = analogRead(0);
-  datos[2]=analogRead(2); 
-  datos[3]=analogRead(3);
+  //datos[2]=analogRead(2); 
+ // datos[3]=analogRead(3);
   bool ok = radio.write(datos, sizeof(datos));
   Serial.println(datos[0]);
 }
